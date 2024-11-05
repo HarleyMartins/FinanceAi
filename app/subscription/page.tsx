cec574cc19eed4ef092a7e334f36c91a2daedb7f
HarleyMartins <harleyfla05@gmail.com>
2024-11-05 20:11:59 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const Subscription = () => {
+    return (<h1>Página de inscrições</h1>)
+}
+
+export default Subscription
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transaction = () => {
+    return (<h1>Página de Transações</h1>)
+}
+
+export default Transaction
